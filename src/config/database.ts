6a34815baf33792d2f402e27a1001a85c6416004
jerryChas <jerry.olsson@chasacademy.se>
2024-09-06 12:34:09 +0200

    efsfe

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,35 +1,13 @@
+import pkg from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-
-const {Pool} = require('pg')
-
-// Define connection details
-export const pool = new Pool({
-  host: 'localhost',          // PostgreSQL server hostname
-  port: 5432,                 // Default port for PostgreSQL
-  user: 'postgres',           // Your PostgreSQL username
-  password: 'postgres',       // Your PostgreSQL password
-  database: 'Ansiktsbok'      // The database you want to connect to
+// <🦝----✨----✨-----🍋-✨----- Connection ------✨-🍋----✨----✨----🦝>
+const pool = new pkg.Pool({
+  connectionString: process.env.DB_URL
 });
 
-// export const dbCheck = async () => {
-//   const client = await pool.connect();
 
-//   try {
-//     // Query the database
-//     const result = await client.query('SELECT * FROM users');
-//     // Access the data correctly
-//     const data = result.rows
-//     // [0]['current_user'];
-//     console.log('Current user:', data);
-//   } catch (err) {
-//     console.error('Error executing query', err);
-//   } finally {
-//     // Release the client back to the pool
-//     client.release();
-//   }
-// };
 
-// // Run the function
-// dbCheck();
-
+export default pool 
--- src/server.ts
+++ src/server.ts
@@ -13,34 +13,3 @@ app.listen(PORT, () => {
 });
 
 
-// // ROUTES ฅᨐฅ 🛣️
-// app.get("/", (req, res) => {
-//     res.send('Hello World! första sidan');
-// })
-
-// // SKAPA EN ANVÄNDARE ฅᨐฅ 🚼
-// app.post("/api/users", async (req, res) => {
-// })
-
-// ELIMINERA EN ANVÄNDARE ฅᨐฅ 🗡️
-// app.delete("/api/users/:id", async (req, res) => {
-// const id = req.params.id;
-
-// try {
-//     const result = await pool.query(
-//         'DELETE FROM users WHERE $1 = id RETURNING *', [id]
-//     );
-//     res.status(200).json(result.rows);
-
-// } catch (err: any) {
-
-//     console.error(err.message);
-//     res.status(500).json({ error: err.message });
-// }
-
-// })
-
-// ÄNDRA EN ANVÄNDARE ฅᨐฅ ✂️
-// app.put("/api/users/:id", async (req, res) => {
-
-// })
